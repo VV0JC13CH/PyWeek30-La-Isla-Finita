d316ad6e73f7aeae6886e0f57c00cc764aada23e
Wojciech Bobrowski <wojciech@bitstudio.dev>
2020-09-20 11:16:44 +0200

    Converted patches to cross-platform form

--- src/assets.py
+++ src/assets.py
@@ -1,17 +1,23 @@
 import arcade
+from pathlib import Path
+
+
+def path_to_string(directory, file):
+    return str(Path.cwd().joinpath(directory, file).resolve())
+
 
 UI_SCALING = 0.5
 
+
 # INTRO
-intro_authors = arcade.load_texture("gfx/intro_team_white.png")
+intro_authors = arcade.load_texture(path_to_string('gfx', 'intro_team_white.png'))
 
 # UI
 
-player_cursor_idle = arcade.Sprite("gfx/cursor_idle.png", UI_SCALING)
-player_cursor_hover = arcade.Sprite("gfx/cursor_hover.png", UI_SCALING)
-player_cursor_select = arcade.Sprite("gfx/cursor_select.png", UI_SCALING)
+player_cursor_idle = arcade.Sprite(path_to_string('gfx', 'cursor_idle.png'), UI_SCALING)
+player_cursor_hover = arcade.Sprite(path_to_string('gfx', 'cursor_hover.png'), UI_SCALING)
+player_cursor_select = arcade.Sprite(path_to_string('gfx', 'cursor_select.png'), UI_SCALING)
 
 # STRINGS
-
 # GAME
-game_sky = arcade.load_texture("gfx/game_sky.png")
+game_sky = arcade.load_texture(path_to_string('gfx', 'game_sky.png'))
--- src/settings.py
+++ src/settings.py
@@ -3,10 +3,14 @@ from pathlib import Path
 import semidbm as db
 
 
+def path_to_string(directory, file):
+    return str(Path.cwd().joinpath(directory, file).resolve())
+
+
 def settings_load():
     # Let's load some data
     _settings = Settings.ConfigParser()
-    _settings.read(str(Path("data/settings.ini").resolve()))
+    _settings.read(path_to_string('data', 'settings.ini'))
     return _settings
 
 
@@ -14,17 +18,17 @@ def setting_change(section, option, value):
     # Let's change some data
     _loaded_settings = settings_load()
     _loaded_settings.set(section, option, value)
-    with open(str(Path("data/settings.ini").resolve()), 'w') as _settings_file:
+    with open(path_to_string('data', 'settings.ini'), 'w') as _settings_file:
         _loaded_settings.write(_settings_file)
 
 
 def save_data(slot, data):
-    new_data = db.open(str(Path("saves/"+slot).resolve()), 'c')
+    new_data = db.open(path_to_string('saves', slot), 'c')
     for (key, value) in data:
         new_data[key] = value
     new_data.close()
 
 
 def load_data(slot):
-    data = db.open(str(Path("saves/"+slot).resolve()), 'r')
+    data = db.open(path_to_string('saves', slot), 'r')
     return data
